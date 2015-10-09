6d52ad5efbe8f6c1c5fc022c6363986c5c5b54db
Janko Marohnić <janko.marohnic@gmail.com>
2015-10-09 21:19:12 +0200

    Document the hooks plugin

--- lib/shrine/plugins/hooks.rb
+++ lib/shrine/plugins/hooks.rb
@@ -1,5 +1,65 @@
 class Shrine
   module Plugins
+    # The hooks plugin allows you to trigger some code before/after
+    # uploading/processing/storing/deleting of each file.
+    #
+    #     plugin :hooks
+    #
+    # Shrine uses instance methods for hooks. To define a hook for an uploader,
+    # you just add an instance method to the uploader:
+    #
+    #     class ImageUploader < Shrine
+    #       def around_process(io, context)
+    #         super
+    #       rescue
+    #         ExceptionNotifier.processing_failed(io, context)
+    #       end
+    #     end
+    #
+    # Each hook will be called with 2 arguments, `io` and `context`.  It's
+    # generally good to always call super when overriding a hook, especially if
+    # you're using inheritance with your uploaders.
+    #
+    # Shrine calls hooks in the following order when uploading a file:
+    #
+    # * `around_upload`
+    #   * `before_upload`
+    #   * `around_process`
+    #     * `before_process`
+    #     * PROCESS
+    #     # `after_process`
+    #   * `around_store`
+    #     * `before_store`
+    #     * STORE
+    #     * `after_store`
+    #   * `after_upload`
+    #
+    # Shrine calls hooks in the following order when deleting a file:
+    #
+    # * `around_delete`
+    #   * `before_delete`
+    #   * DELETE
+    #   * `after_delete`
+    #
+    # It may be useful to know that you can realize some form of communication
+    # between the hooks; whatever you save to the `context` hash will be
+    # forwarded further down:
+    #
+    #     class ImageUploader < Shrine
+    #       def before_upload(io, context)
+    #         context[:_foo] = "bar"
+    #         super
+    #       end
+    #
+    #       def before_process(io, context)
+    #         context[:_foo] #=> "bar"
+    #         super
+    #       end
+    #     end
+    #
+    # Note that in that case you should always somehow mark this key as private
+    # (for example with an underscore) so that it doesn't clash with any
+    # existing keys.
     module Hooks
       module InstanceMethods
         def upload(io, context = {})
