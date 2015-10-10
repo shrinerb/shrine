ce992ebb816045ae72eb3609d361f835d80f80a1
Janko Marohnić <janko.marohnic@gmail.com>
2015-10-11 00:16:27 +0200

    Document the recache plugin

--- lib/shrine/plugins/recache.rb
+++ lib/shrine/plugins/recache.rb
@@ -1,5 +1,25 @@
 class Shrine
   module Plugins
+    # The recache plugin allows you to process your attachment after
+    # validations succeed, but before the attachment is promoted. This is
+    # useful for example when you want to generate some versions upfront (so
+    # the user immediately sees them) and other versions you want to generate
+    # in the promotion phase in a background job.
+    #
+    # The phase will be set to `:recache`:
+    #
+    #     class ImageUploader
+    #       plugin :recache
+    #
+    #       def process(io, context)
+    #         case context[:phase]
+    #         when :recache
+    #           # generate cheap versions
+    #         when :promote
+    #           # generate more expensive versions
+    #         end
+    #       end
+    #     end
     module Recache
       module AttacherMethods
         def save
