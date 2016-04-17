6ee8d07c369d67b1c2c90b0d38e7377e56cf4256
Janko Marohnić <janko.marohnic@gmail.com>
2016-04-18 01:29:52 +0200

    Refactor ORM plugin tests

--- test/plugin/activerecord_test.rb
+++ test/plugin/activerecord_test.rb
@@ -27,111 +27,136 @@ describe "the activerecord plugin" do
     Object.send(:remove_const, "User")
   end
 
-  it "promotes on save" do
-    @user.update(avatar: fakeio("file1")) # insert
-    refute @user.changed?
-    assert_equal "store", @user.avatar.storage_key
-    assert_equal "file1", @user.avatar.read
-
-    @user.update(avatar: fakeio("file2")) # update
-    refute @user.changed?
-    assert_equal "store", @user.avatar.storage_key
-    assert_equal "file2", @user.avatar.read
-  end
-
-  it "successfully promotes when record is invalid" do
-    @user.class.validate { errors.add(:base, "Invalid") }
-    @user.avatar = fakeio
-    @user.save(validate: false)
-    refute @user.changed?
-    assert_equal "store", @user.avatar.storage_key
-  end
-
-  it "adds validation errors to the record" do
-    @user.avatar_attacher.class.validate { errors << "error" }
-    @user.avatar = fakeio
-    refute @user.valid?
-    assert_equal Hash[avatar: ["error"]], @user.errors.to_hash
-  end
-
-  it "replaces after saving" do
-    @user.update(avatar: fakeio)
-    uploaded_file = @user.avatar
-    @user.update(avatar: fakeio)
-    refute uploaded_file.exists?
-  end
-
-  it "doesn't replace if callback chain halted" do
-    @user.update(avatar: fakeio)
-    uploaded_file = @user.avatar
-    @user.class.before_save { false }
-    @user.update(avatar: fakeio)
-    assert uploaded_file.exists?
-  end
-
-  it "triggers saving if file was attached" do
-    @user.avatar_attacher.expects(:save).twice
-    @user.update(avatar: fakeio) # insert
-    @user.update(avatar: fakeio) # update
-  end
-
-  it "doesn't trigger saving if file wasn't attached" do
-    @user.avatar_attacher.expects(:save).never
-    @user.save # insert
-    @user.save # update
-  end
-
-  it "destroys attachments" do
-    @user.update(avatar: fakeio)
-    @user.destroy
-    refute @user.avatar.exists?
-  end
-
-  it "works with backgrounding plugin" do
-    @uploader.class.plugin :backgrounding
-    @attacher.class.promote { |data| self.class.promote(data) }
-    @attacher.class.delete { |data| self.class.delete(data) }
-
-    @user.update(avatar: fakeio)
-    assert_equal "store", @user.reload.avatar.storage_key
-
-    @user.destroy
-    refute @user.avatar.exists?
-  end
-
-  it "doesn't raise errors in background job when record is not found" do
-    @uploader.class.plugin :backgrounding
-    @attacher.class.promote { |data| record.delete; self.class.promote(data) }
-    @attacher.class.delete { |data| record.delete; self.class.delete(data) }
-    @user.update(avatar: fakeio)
-    @user.destroy
-  end
-
-  it "doesn't send another background job when saved again" do
-    @uploader.class.plugin :backgrounding
-    @attacher.class.promote { |data| @f = Fiber.new{self.class.promote(data)} }
-    @user.update(avatar: fakeio)
-    fiber = @user.avatar_attacher.instance_variable_get("@f")
-    @user.save
-    assert_equal fiber, @user.avatar_attacher.instance_variable_get("@f")
-  end
-
-  it "terminates swapping if attachment has changed" do
-    @attacher.instance_eval do
-      def swap(uploaded_file)
-        record.class.update_all(avatar_data: nil)
-        super
-      end
+  describe "validating" do
+    it "adds errors to the record" do
+      @user.avatar_attacher.class.validate { errors << "error" }
+      @user.avatar = fakeio
+      refute @user.valid?
+      assert_equal Hash[avatar: ["error"]], @user.errors.to_hash
     end
-    @user.update(avatar: fakeio)
-    assert_equal nil, @user.reload.avatar
+  end
 
-    @attacher.instance_eval do
-      def swap(uploaded_file)
-        record.class.delete_all
-        super
-      end
+  describe "promoting" do
+    it "is triggered on save" do
+      @user.update(avatar: fakeio("file1")) # insert
+      refute @user.changed?
+      assert_equal "store", @user.avatar.storage_key
+      assert_equal "file1", @user.avatar.read
+
+      @user.update(avatar: fakeio("file2")) # update
+      refute @user.changed?
+      assert_equal "store", @user.avatar.storage_key
+      assert_equal "file2", @user.avatar.read
     end
-    @user.update(avatar: fakeio)
+
+    it "triggers callbacks" do
+      @user.class.before_save do
+        @promote_callback = true if avatar.storage_key == "store"
+      end
+      @user.update(avatar: fakeio)
+      assert @user.instance_variable_get("@promote_callback")
+    end
+
+    it "bypasses validations" do
+      @user.class.validate { errors.add(:base, "Invalid") }
+      @user.avatar = fakeio
+      @user.save(validate: false)
+      refute @user.changed?
+      assert_equal "store", @user.avatar.storage_key
+    end
+
+    it "works with backgrounding" do
+      @uploader.class.plugin :backgrounding
+      @attacher.class.promote { |data| self.class.promote(data) }
+      @user.update(avatar: fakeio)
+      assert_equal "store", @user.reload.avatar.storage_key
+    end
+
+    it "is terminated when attachment changed before update" do
+      @attacher.instance_eval do
+        def update(*)
+          record.class.update_all(avatar_data: nil)
+          super
+        end
+      end
+      @user.update(avatar: fakeio)
+      assert_equal nil, @user.reload.avatar
+    end
+
+    it "is terminated when record was deleted" do
+      @attacher.instance_eval do
+        def update(uploaded_file)
+          record.class.delete_all
+          super
+        end
+      end
+      @user.update(avatar: fakeio)
+    end
+  end
+
+  describe "replacing" do
+    it "is triggered after saving" do
+      @user.update(avatar: fakeio)
+      uploaded_file = @user.avatar
+      @user.update(avatar: fakeio)
+      refute uploaded_file.exists?
+    end
+
+    it "isn't triggered when callback chain is halted" do
+      @user.update(avatar: fakeio)
+      uploaded_file = @user.avatar
+      @user.class.before_save { false }
+      @user.update(avatar: fakeio)
+      assert uploaded_file.exists?
+    end
+  end
+
+  describe "saving" do
+    it "is triggered when file is attached" do
+      @user.avatar_attacher.expects(:save).twice
+      @user.update(avatar: fakeio) # insert
+      @user.update(avatar: fakeio) # update
+    end
+
+    it "isn't triggered when no file was attached" do
+      @user.avatar_attacher.expects(:save).never
+      @user.save # insert
+      @user.save # update
+    end
+  end
+
+  describe "destroying" do
+    it "is triggered on record destroy" do
+      @user.update(avatar: fakeio)
+      @user.destroy
+      refute @user.avatar.exists?
+    end
+
+    it "works with backgrounding" do
+      @uploader.class.plugin :backgrounding
+      @attacher.class.delete { |data| self.class.delete(data) }
+      @user.update(avatar: fakeio)
+      @user.destroy
+      refute @user.avatar.exists?
+    end
+  end
+
+  describe "backgrounding" do
+    it "doesn't raise errors when record wasn't found" do
+      @uploader.class.plugin :backgrounding
+      @attacher.class.promote { |data| record.delete; self.class.promote(data) }
+      @attacher.class.delete { |data| record.delete; self.class.delete(data) }
+      @user.update(avatar: fakeio)
+      @user.destroy
+    end
+
+    it "is triggered only when attachment has changed" do
+      @uploader.class.plugin :backgrounding
+      @attacher.class.promote { |data| @f = Fiber.new{self.class.promote(data)} }
+      @user.update(avatar: fakeio)
+      fiber = @user.avatar_attacher.instance_variable_get("@f")
+      @user.save
+      assert_equal fiber, @user.avatar_attacher.instance_variable_get("@f")
+    end
   end
 end
--- test/plugin/sequel_test.rb
+++ test/plugin/sequel_test.rb
@@ -26,119 +26,147 @@ describe "the sequel plugin" do
     Object.send(:remove_const, "User")
   end
 
-  it "promotes on save" do
-    @user.update(avatar: fakeio("file1")) # insert
-    refute @user.modified?
-    assert_equal "store", @user.avatar.storage_key
-    assert_equal "file1", @user.avatar.read
-
-    @user.update(avatar: fakeio("file2")) # update
-    refute @user.modified?
-    assert_equal "store", @user.avatar.storage_key
-    assert_equal "file2", @user.avatar.read
-  end
-
-  it "successfully promotes when record is invalid" do
-    @user.instance_eval { def validate; errors.add(:base, "Invalid"); end }
-    @user.avatar = fakeio
-    @user.save(validate: false)
-    assert_empty @user.changed_columns
-    assert_equal "store", @user.avatar.storage_key
-  end
-
-  it "adds validation errors to the record" do
-    @user.avatar_attacher.class.validate { errors << "error" }
-    @user.avatar = fakeio
-    refute @user.valid?
-    assert_equal Hash[avatar: ["error"]], @user.errors.to_hash
-  end
-
-  it "replaces after saving" do
-    @user.update(avatar: fakeio)
-    uploaded_file = @user.avatar
-    @user.update(avatar: fakeio)
-    refute uploaded_file.exists?
-  end
-
-  it "doesn't replace if callback chain halted" do
-    @user.update(avatar: fakeio)
-    uploaded_file = @user.avatar
-    @user.instance_eval { def before_save; cancel_action; end }
-    @user.update(avatar: fakeio) rescue nil
-    assert uploaded_file.exists?
-  end
-
-  it "triggers saving if file was attached" do
-    @user.avatar_attacher.expects(:save).twice
-    @user.update(avatar: fakeio) # insert
-    @user.update(avatar: fakeio) # update
-  end
-
-  it "doesn't trigger saving if file wasn't attached" do
-    @user.avatar_attacher.expects(:save).never
-    @user.save # insert
-    @user.save # update
-  end
-
-  it "destroys attachments" do
-    @user.update(avatar: fakeio)
-    @user.destroy
-    refute @user.avatar.exists?
-  end
-
-  it "works with backgrounding plugin" do
-    @uploader.class.plugin :backgrounding
-    @attacher.class.promote { |data| self.class.promote(data) }
-    @attacher.class.delete { |data| self.class.delete(data) }
-
-    @user.update(avatar: fakeio)
-    assert_equal "store", @user.reload.avatar.storage_key
-
-    @user.destroy
-    refute @user.avatar.exists?
-  end
-
-  it "doesn't raise errors in background job when record is not found" do
-    @uploader.class.plugin :backgrounding
-    @attacher.class.promote { |data| @f = Fiber.new{self.class.promote(data)} }
-    @attacher.class.delete { |data| @f = Fiber.new{self.class.delete(data)} }
-
-    @user.update(avatar: fakeio)
-    @user.delete
-    @user.avatar_attacher.instance_variable_get("@f").resume
-    @user = @user.class.new
-
-    @user.update(avatar_data: @user.avatar_attacher.store.upload(fakeio).to_json)
-    @user.destroy
-    @user.avatar_attacher.instance_variable_get("@f").resume
-  end
-
-  it "doesn't send another background job when saved again" do
-    @uploader.class.plugin :backgrounding
-    @attacher.class.promote { |data| @f = Fiber.new{self.class.promote(data)} }
-    @user.update(avatar: fakeio)
-    fiber = @user.avatar_attacher.instance_variable_get("@f")
-    @user.save
-    assert_equal fiber, @user.avatar_attacher.instance_variable_get("@f")
-  end
-
-  it "terminates swapping if attachment has changed" do
-    @attacher.instance_eval do
-      def swap(uploaded_file)
-        record.this.update(avatar_data: nil)
-        super
-      end
+  describe "validating" do
+    it "adds validation errors to the record" do
+      @user.avatar_attacher.class.validate { errors << "error" }
+      @user.avatar = fakeio
+      refute @user.valid?
+      assert_equal Hash[avatar: ["error"]], @user.errors.to_hash
     end
-    @user.update(avatar: fakeio)
-    assert_equal nil, @user.reload.avatar
+  end
 
-    @attacher.instance_eval do
-      def swap(uploaded_file)
-        record.this.delete
-        super
-      end
+  describe "promoting" do
+    it "is triggered on save" do
+      @user.update(avatar: fakeio("file1")) # insert
+      refute @user.modified?
+      assert_equal "store", @user.avatar.storage_key
+      assert_equal "file1", @user.avatar.read
+
+      @user.update(avatar: fakeio("file2")) # update
+      refute @user.modified?
+      assert_equal "store", @user.avatar.storage_key
+      assert_equal "file2", @user.avatar.read
     end
-    @user.update(avatar: fakeio)
+
+    it "triggers callbacks" do
+      @user.instance_eval do
+        def before_save
+          @promote_callback = true if avatar.storage_key == "store"
+          super
+        end
+      end
+      @user.update(avatar: fakeio)
+      assert @user.instance_variable_get("@promote_callback")
+    end
+
+    it "bypasses validations" do
+      @user.instance_eval { def validate; errors.add(:base, "Invalid"); end }
+      @user.avatar = fakeio
+      @user.save(validate: false)
+      assert_empty @user.changed_columns
+      assert_equal "store", @user.avatar.storage_key
+    end
+
+    it "works with backgrounding" do
+      @uploader.class.plugin :backgrounding
+      @attacher.class.promote { |data| self.class.promote(data) }
+      @user.update(avatar: fakeio)
+      assert_equal "store", @user.reload.avatar.storage_key
+    end
+
+    it "is terminated when attachment changed before update" do
+      @attacher.instance_eval do
+        def update(*)
+          record.this.update(avatar_data: nil)
+          super
+        end
+      end
+      @user.update(avatar: fakeio)
+      assert_equal nil, @user.reload.avatar
+    end
+
+    it "is terminated when record was deleted" do
+      @attacher.instance_eval do
+        def update(uploaded_file)
+          record.this.delete
+          super
+        end
+      end
+      @user.update(avatar: fakeio)
+    end
+  end
+
+  describe "replacing" do
+    it "is triggered on save" do
+      @user.update(avatar: fakeio)
+      uploaded_file = @user.avatar
+      @user.update(avatar: fakeio)
+      refute uploaded_file.exists?
+    end
+
+    it "is terminated when callback chain is halted" do
+      @user.update(avatar: fakeio)
+      uploaded_file = @user.avatar
+      @user.instance_eval { def before_save; cancel_action; end }
+      @user.update(avatar: fakeio) rescue nil
+      assert uploaded_file.exists?
+    end
+  end
+
+  describe "saving" do
+    it "is triggered when file is attached" do
+      @user.avatar_attacher.expects(:save).twice
+      @user.update(avatar: fakeio) # insert
+      @user.update(avatar: fakeio) # update
+    end
+
+    it "isn't triggered when no file was attached" do
+      @user.avatar_attacher.expects(:save).never
+      @user.save # insert
+      @user.save # update
+    end
+  end
+
+  describe "destroying" do
+    it "is triggered on record destroy" do
+      @user.update(avatar: fakeio)
+      @user.destroy
+      refute @user.avatar.exists?
+    end
+
+    it "works with backgrounding" do
+      @uploader.class.plugin :backgrounding
+      @attacher.class.delete { |data| self.class.delete(data) }
+      @user.update(avatar: fakeio)
+      @user.destroy
+      refute @user.avatar.exists?
+    end
+  end
+
+  describe "backgrounding" do
+    it "doesn't raise errors when record wasn't found" do
+      @uploader.class.plugin :backgrounding
+      @attacher.class.promote { |data| @f = Fiber.new{self.class.promote(data)} }
+      @attacher.class.delete { |data| @f = Fiber.new{self.class.delete(data)} }
+
+      @user.update(avatar: fakeio)
+      @user.delete
+      @user.avatar_attacher.instance_variable_get("@f").resume
+      @user = @user.class.new
+
+      @user.update(avatar_data: @user.avatar_attacher.store.upload(fakeio).to_json)
+      @user.destroy
+      @user.avatar_attacher.instance_variable_get("@f").resume
+    end
+
+    it "is triggered only when attachment has changed" do
+      @uploader.class.plugin :backgrounding
+      @attacher.class.promote { |data| @f = Fiber.new{self.class.promote(data)} }
+      @user.update(avatar: fakeio)
+      fiber = @user.avatar_attacher.instance_variable_get("@f")
+      @user.save
+      assert_equal fiber, @user.avatar_attacher.instance_variable_get("@f")
+    end
   end
 
   it "adds support for Postgres JSON columns" do
