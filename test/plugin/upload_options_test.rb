ce5e2a16b5d347b2b64f9f21960ba2a043338d72
Janko Marohnić <janko.marohnic@gmail.com>
2016-10-24 12:57:23 +0800

    Improve upload_options plugin tests

--- test/plugin/upload_options_test.rb
+++ test/plugin/upload_options_test.rb
@@ -9,38 +9,26 @@ describe Shrine::Plugins::UploadOptions do
   end
 
   it "accepts a block" do
-    @uploader.opts[:upload_options] = {store: ->(io, context){Hash[foo: "bar"]}}
-    @uploader.storage.instance_eval do
-      def upload(io, id, shrine_metadata: {}, **upload_options)
-        upload_options.fetch(:foo)
-        super
-      end
-    end
-
+    @uploader.opts[:upload_options] = {store: ->(io, context){Hash[foo: "foo"]}}
+    @uploader.storage.expects(:upload).with { |*, **options| options[:foo] == "foo" }
     @uploader.upload(fakeio)
   end
 
   it "accepts a hash" do
-    @uploader.opts[:upload_options] = {store: {foo: "bar"}}
-    @uploader.storage.instance_eval do
-      def upload(io, id, shrine_metadata: {}, **upload_options)
-        upload_options.fetch(:foo)
-        super
-      end
-    end
-
+    @uploader.opts[:upload_options] = {store: {foo: "foo"}}
+    @uploader.storage.expects(:upload).with { |*, **options| options[:foo] == "foo" }
     @uploader.upload(fakeio)
   end
 
   it "only passes upload options to specified storages" do
-    @uploader.opts[:upload_options] = {cache: {foo: "bar"}}
-    @uploader.storage.instance_eval do
-      def upload(io, id, shrine_metadata: {}, **upload_options)
-        raise if upload_options.any?
-        super
-      end
-    end
-
+    @uploader.opts[:upload_options] = {cache: {foo: "foo"}}
+    @uploader.storage.expects(:upload).with { |*, **options| !options.key?(:foo) }
     @uploader.upload(fakeio)
+  end
+
+  it "takes lower precedence than :upload_options" do
+    @uploader.opts[:upload_options] = {cache: {foo: "foo"}}
+    @uploader.storage.expects(:upload).with { |*, **options| options[:foo] == "bar" }
+    @uploader.upload(fakeio, upload_options: {foo: "bar"})
   end
 end
