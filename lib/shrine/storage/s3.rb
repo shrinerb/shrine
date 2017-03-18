a8d78a1d49fa4a8ad13779a0a30713ebdedbdaa4
Janko Marohnić <janko.marohnic@gmail.com>
2017-03-19 04:17:11 +1100

    Use "shrine-s3" name for Tempfile instead of "s3"

--- lib/shrine/storage/s3.rb
+++ lib/shrine/storage/s3.rb
@@ -210,7 +210,7 @@ class Shrine
 
       # Downloads the file from S3, and returns a `Tempfile`.
       def download(id)
-        tempfile = Tempfile.new(["s3", File.extname(id)], binmode: true)
+        tempfile = Tempfile.new(["shrine-s3", File.extname(id)], binmode: true)
         (object = object(id)).get(response_target: tempfile.path)
         tempfile.singleton_class.instance_eval { attr_accessor :content_type }
         tempfile.content_type = object.content_type
