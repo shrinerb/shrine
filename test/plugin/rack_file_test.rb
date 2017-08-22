50d7b525b0ca4478c7e69ce631d8e24aeabca40d
Janko Marohnić <janko.marohnic@gmail.com>
2017-08-22 18:37:15 +0200

    Use Shrine.rack_file in rack_file plugin tests

--- test/plugin/rack_file_test.rb
+++ test/plugin/rack_file_test.rb
@@ -4,8 +4,9 @@ require "tempfile"
 
 describe Shrine::Plugins::RackFile do
   before do
-    @attacher = attacher { plugin :rack_file }
-    @rack_file = {
+    @attacher  = attacher { plugin :rack_file }
+    @shrine    = @attacher.shrine_class
+    @rack_hash = {
       name: "file",
       tempfile: Tempfile.new(""),
       filename: "image.jpg",
@@ -14,17 +15,34 @@ describe Shrine::Plugins::RackFile do
     }
   end
 
-  it "enables assignment of Rack file hashes" do
-    @attacher.assign(@rack_file)
-    cached_file = @attacher.get
+  it "allows converting Rack file hash into an IO object" do
+    rack_file = @shrine.rack_file(@rack_hash)
 
-    assert_equal "",           cached_file.read
-    assert_equal 0,            cached_file.size
-    assert_equal "image.jpg",  cached_file.original_filename
-    assert_equal "image/jpeg", cached_file.mime_type
+    assert_equal "",   rack_file.read
+    assert_equal 0,    rack_file.size
+    assert_equal true, rack_file.eof?
+
+    rack_file.rewind
+    rack_file.close
   end
 
-  it "accepts hash with indifferent access like objects" do
+  it "exposes metadata on the IO object" do
+    rack_file = @shrine.rack_file(@rack_hash)
+
+    assert_equal 0,                          rack_file.size
+    assert_equal "image.jpg",                rack_file.original_filename
+    assert_equal "image/jpeg",               rack_file.content_type
+  end
+
+  it "adds methods for accessing the Tempfile" do
+    rack_file = @shrine.rack_file(@rack_hash)
+
+    assert_equal @rack_hash[:tempfile],      rack_file.tempfile
+    assert_equal @rack_hash[:tempfile],      rack_file.to_io
+    assert_equal @rack_hash[:tempfile].path, rack_file.path
+  end
+
+  it "accepts hash-like parameters" do
     hash_like_class = Class.new(Hash) do
       def initialize(hash)
         @hash = hash.inject({}) { |h, (key, value)| h.update(key.to_s => value) }
@@ -39,28 +57,24 @@ describe Shrine::Plugins::RackFile do
       end
     end
 
-    hash_like_object = hash_like_class.new(@rack_file)
-    @attacher.assign(hash_like_object)
-    cached_file = @attacher.get
+    rack_file = @shrine.rack_file(hash_like_class.new(@rack_hash))
 
-    assert_equal "",           cached_file.read
-    assert_equal 0,            cached_file.size
-    assert_equal "image.jpg",  cached_file.original_filename
-    assert_equal "image/jpeg", cached_file.mime_type
+    assert_equal 0,            rack_file.size
+    assert_equal "image.jpg",  rack_file.original_filename
+    assert_equal "image/jpeg", rack_file.content_type
   end
 
-  it "adds #path, #to_io and #tempfile methods to IO" do
-    @attacher.cache.instance_eval { def process(io, context) @rack_file = io end }
-    @attacher.assign(@rack_file)
-    rack_file = @attacher.cache.instance_variable_get("@rack_file")
+  it "supports attaching Rack files directly" do
+    @attacher.assign(@rack_hash)
 
-    assert_equal @rack_file[:tempfile].path, rack_file.path
-    assert_equal @rack_file[:tempfile],      rack_file.to_io
-    assert_equal @rack_file[:tempfile],      rack_file.tempfile
+    assert_equal "",           @attacher.get.read
+    assert_equal 0,            @attacher.get.size
+    assert_equal "image.jpg",  @attacher.get.original_filename
+    assert_equal "image/jpeg", @attacher.get.mime_type
   end
 
-  deprecated "works with uploader" do
-    uploaded_file = @attacher.store.upload(@rack_file)
+  deprecated "supports uploading Rack files directly" do
+    uploaded_file = @attacher.store.upload(@rack_hash)
 
     assert_equal "",           uploaded_file.read
     assert_equal 0,            uploaded_file.size
