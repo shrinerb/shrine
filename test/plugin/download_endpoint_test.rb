4638c8fdc7e70cbb824badd150ec0e6b9bcefa30
Janko Marohnić <janko.marohnic@gmail.com>
2018-11-16 02:09:04 +0100

    Add :host option for UploadedFile#download_url
    The download_endpoint already has a global :host option. However,
    sometimes it's useful to be able to set the host per URL, for example if
    we want to use multiple CDN hosts to get around browsers' limit of 6
    concurrent requests per host, or if we want to infer the host from the
    request context.
    
    This commit adds the ability to pass :host to UploadedFile#download_url:
    
      uploaded_file.download_url(host: "https://example.com")

--- test/plugin/download_endpoint_test.rb
+++ test/plugin/download_endpoint_test.rb
@@ -10,57 +10,56 @@ describe Shrine::Plugins::DownloadEndpoint do
   before do
     @uploader = uploader { plugin :download_endpoint }
     @shrine = @uploader.class
+    @uploaded_file = @uploader.upload(fakeio)
   end
 
   it "returns a file response" do
     io = fakeio("a" * 16*1024 + "b" * 16*1024 + "c" * 4*1024, content_type: "text/plain", filename: "content.txt")
-    uploaded_file = @uploader.upload(io)
-    response = app.get(uploaded_file.download_url)
+    @uploaded_file = @uploader.upload(io)
+    response = app.get(@uploaded_file.download_url)
     assert_equal 200, response.status
-    assert_equal uploaded_file.read, response.body_binary
-    assert_equal uploaded_file.size.to_s, response.headers["Content-Length"]
-    assert_equal uploaded_file.mime_type, response.headers["Content-Type"]
-    assert_equal "inline; filename=\"#{uploaded_file.original_filename}\"", response.headers["Content-Disposition"]
+    assert_equal @uploaded_file.read, response.body_binary
+    assert_equal @uploaded_file.size.to_s, response.headers["Content-Length"]
+    assert_equal @uploaded_file.mime_type, response.headers["Content-Type"]
+    assert_equal "inline; filename=\"#{@uploaded_file.original_filename}\"", response.headers["Content-Disposition"]
   end
 
   it "applies :disposition to response" do
     @uploader = uploader { plugin :download_endpoint, disposition: "attachment" }
-    uploaded_file = @uploader.upload(fakeio)
-    response = app.get(uploaded_file.download_url)
-    assert_equal "attachment; filename=\"#{uploaded_file.id}\"", response.headers["Content-Disposition"]
+    @uploaded_file = @uploader.upload(fakeio)
+    response = app.get(@uploaded_file.download_url)
+    assert_equal "attachment; filename=\"#{@uploaded_file.id}\"", response.headers["Content-Disposition"]
   end
 
   it "returns Cache-Control" do
-    uploaded_file = @uploader.upload(fakeio)
-    response = app.get(uploaded_file.download_url)
+    response = app.get(@uploaded_file.download_url)
     assert_equal "max-age=31536000", response.headers["Cache-Control"]
   end
 
   it "accepts :redirect with true" do
     @uploader = uploader { plugin :download_endpoint, redirect: true }
-    uploaded_file = @uploader.upload(fakeio)
-    response = app.get(uploaded_file.download_url)
+    @uploaded_file = @uploader.upload(fakeio)
+    response = app.get(@uploaded_file.download_url)
     assert_equal 302, response.status
     assert_match %r{^memory://\w+$}, response.headers["Location"]
   end
 
   it "accepts :redirect with proc" do
     @uploader = uploader { plugin :download_endpoint, redirect: -> (uploaded_file, request) { "/foo" } }
-    uploaded_file = @uploader.upload(fakeio)
-    response = app.get(uploaded_file.download_url)
+    @uploaded_file = @uploader.upload(fakeio)
+    response = app.get(@uploaded_file.download_url)
     assert_equal 302, response.status
     assert_equal "/foo", response.headers["Location"]
   end
 
   it "returns Accept-Ranges" do
-    uploaded_file = @uploader.upload(fakeio)
-    response = app.get(uploaded_file.download_url)
+    response = app.get(@uploaded_file.download_url)
     assert_equal "bytes", response.headers["Accept-Ranges"]
   end
 
   it "supports ranged requests" do
-    uploaded_file = @uploader.upload(fakeio("content"))
-    response = app.get(uploaded_file.download_url, headers: { "Range" => "bytes=2-4" })
+    @uploaded_file = @uploader.upload(fakeio("content"))
+    response = app.get(@uploaded_file.download_url, headers: { "Range" => "bytes=2-4" })
     assert_equal 206,           response.status
     assert_equal "bytes 2-4/7", response.headers["Content-Range"]
     assert_equal "3",           response.headers["Content-Length"]
@@ -68,54 +67,54 @@ describe Shrine::Plugins::DownloadEndpoint do
   end
 
   it "returns 404 for nonexisting file" do
-    uploaded_file = @uploader.upload(fakeio)
-    uploaded_file.data["id"] = "nonexistent"
-    response = app.get(uploaded_file.download_url)
-    assert_equal 404, response.status
+    @uploaded_file.data["id"] = "nonexistent"
+    response = app.get(@uploaded_file.download_url)
+    assert_equal 404,              response.status
     assert_equal "File Not Found", response.body_binary
-    assert_equal "text/plain", response.content_type
+    assert_equal "text/plain",     response.content_type
   end
 
   it "returns 404 for nonexisting storage" do
     uploaded_file = @uploader.upload(fakeio)
     @uploader.class.storages.delete(uploaded_file.storage_key.to_sym)
     response = app.get(uploaded_file.download_url)
-    assert_equal 404, response.status
+    assert_equal 404,              response.status
     assert_equal "File Not Found", response.body_binary
-    assert_equal "text/plain", response.content_type
+    assert_equal "text/plain",     response.content_type
   end
 
   it "adds :host to the URL" do
     @shrine.plugin :download_endpoint, host: "http://example.com"
-    uploaded_file = @uploader.upload(fakeio)
-    assert_match %r{http://example.com/\S+}, uploaded_file.download_url
+    assert_match %r{http://example\.com/\S+}, @uploaded_file.download_url
   end
 
   it "adds :prefix to the URL" do
     @shrine.plugin :download_endpoint, prefix: "attachments"
-    uploaded_file = @uploader.upload(fakeio)
-    assert_match %r{/attachments/\S+}, uploaded_file.download_url
+    assert_match %r{/attachments/\S+}, @uploaded_file.download_url
   end
 
   it "adds :host and :prefix to the URL" do
     @shrine.plugin :download_endpoint, host: "http://example.com", prefix: "attachments"
-    uploaded_file = @uploader.upload(fakeio)
-    assert_match %r{http://example.com/attachments/\S+}, uploaded_file.download_url
+    assert_match %r{http://example\.com/attachments/\S+}, @uploaded_file.download_url
+  end
+
+  it "allows specifying :host per URL" do
+    @shrine.plugin :download_endpoint, host: "http://foo.com"
+    url = @uploaded_file.download_url(host: "http://bar.com")
+    assert_match %r{http://bar\.com/\S+}, url
   end
 
   it "returns same URL regardless of metadata order" do
-    uploaded_file = @uploader.upload(fakeio)
-    uploaded_file.data["metadata"] = { "filename" => "a", "mime_type" => "b", "size" => "c" }
-    url1 = uploaded_file.url
-    uploaded_file.data["metadata"] = { "mime_type" => "b", "size" => "c", "filename" => "a" }
-    url2 = uploaded_file.url
+    @uploaded_file.data["metadata"] = { "filename" => "a", "mime_type" => "b", "size" => "c" }
+    url1 = @uploaded_file.url
+    @uploaded_file.data["metadata"] = { "mime_type" => "b", "size" => "c", "filename" => "a" }
+    url2 = @uploaded_file.url
     assert_equal url1, url2
   end
 
   it "supports legacy URLs" do
-    uploaded_file = @uploader.upload(fakeio)
-    response = app.get("/#{uploaded_file.storage_key}/#{uploaded_file.id}")
-    assert_equal uploaded_file.read, response.body_binary
+    response = app.get("/#{@uploaded_file.storage_key}/#{@uploaded_file.id}")
+    assert_equal @uploaded_file.read, response.body_binary
   end
 
   it "makes the endpoint inheritable" do
