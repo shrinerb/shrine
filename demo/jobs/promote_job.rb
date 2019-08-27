66b8082ee082d3ec0b79f05735de068208e85e48
Janko Marohnić <janko.marohnic@gmail.com>
2019-08-27 20:32:26 +0200

    Demo app fixes

--- demo/jobs/promote_job.rb
+++ demo/jobs/promote_job.rb
@@ -4,11 +4,10 @@ class PromoteJob
   include SuckerPunch::Job
 
   def perform(record_class, record_id, name, data)
-    record_class = Object.const_get(record_class.to_s)
-    record       = record_class.with_pk!(record_id)
-
+    record   = Object.const_get(record_class.to_s).with_pk!(record_id)
     attacher = Shrine::Attacher.retrieve(model: record, name: name, data: data)
-    attacher.create_derivatives(:thumbnails) if record_class == Album
+
+    attacher.create_derivatives(:thumbnails) if record.is_a?(Album)
     attacher.atomic_promote
   rescue Shrine::AttachmentChanged
     # attachment has changed, so nothing to do
