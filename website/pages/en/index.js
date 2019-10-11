f7f0acd769b3030f9ea56e646cfca3d60262dcea
Janko Marohnić <janko.marohnic@gmail.com>
2019-10-11 12:34:47 +0200

    Move README.md content into doc/getting_started.md
    This is needed because Docusaurus cannot show documents outside of the
    doc/ directory. Now the website landing page will show the highlights,
    while the doc/getting_started.md document will go straight to setup. The
    README will then just show basic links.

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -79,10 +79,53 @@ class Index extends React.Component {
     const {config: siteConfig, language = ''} = this.props;
     const {baseUrl} = siteConfig;
 
+    const Features = () => (
+      <Container>
+        <MarkdownBlock>{`
+* **Modular design** – the [plugin system] allows you to load only the functionality you need
+* **Memory friendly** – streaming uploads and [downloads][Retrieving Uploads] make it work great with large files
+* **Cloud storage** – store files on [disk][FileSystem], [AWS S3][S3], [Google Cloud][GCS], [Cloudinary] and others
+* **Persistence integrations** – works with [Sequel], [ActiveRecord], [ROM], [Hanami] and [Mongoid] and others
+* **Flexible processing** – generate thumbnails [up front] or [on-the-fly] using [ImageMagick] or [libvips]
+* **Metadata validation** – [validate files][validation] based on [extracted metadata][metadata]
+* **Direct uploads** – upload asynchronously [to your app][simple upload] or [to the cloud][presigned upload] using [Uppy]
+* **Resumable uploads** – make large file uploads [resumable][resumable upload] on [S3][uppy-s3_multipart] or [tus][tus-ruby-server]
+* **Background jobs** – built-in support for [background processing][backgrounding] that supports [any backgrounding library][Backgrounding Libraries]
+
+[plugin system]: /docs/getting-started#plugin-system
+[Retrieving Uploads]: /docs/retrieving-uploads
+[FileSystem]: /docs/storage/file-system
+[S3]: /docs/storage/s3
+[GCS]: https://github.com/renchap/shrine-google_cloud_storage
+[Cloudinary]: https://github.com/shrinerb/shrine-cloudinary
+[Sequel]: /docs/plugins/sequel
+[ActiveRecord]: /docs/plugins/activerecord
+[ROM]: https://github.com/shrinerb/shrine-rom
+[Hanami]: https://github.com/katafrakt/hanami-shrine
+[Mongoid]: https://github.com/shrinerb/shrine-mongoid
+[up front]: /docs/getting-started#processing-up-front
+[on-the-fly]: /docs/getting-started#processing-on-the-fly
+[ImageMagick]: https://github.com/janko/image_processing/blob/master/doc/minimagick.md#readme
+[libvips]: https://github.com/janko/image_processing/blob/master/doc/vips.md#readme
+[validation]: /docs/validation
+[metadata]: /docs/metadata
+[simple upload]: /docs/getting-started#simple-direct-upload
+[presigned upload]: /docs/getting-started#presigned-direct-upload
+[resumable upload]: /docs/getting-started#resumable-direct-upload
+[Uppy]: https://uppy.io/
+[uppy-s3_multipart]: https://github.com/janko/uppy-s3_multipart
+[tus-ruby-server]: https://github.com/janko/tus-ruby-server
+[backgrounding]: /docs/plugins/backgrounding
+[Backgrounding Libraries]: https://github.com/shrinerb/shrine/wiki/Backgrounding-Libraries
+        `}</MarkdownBlock>
+      </Container>
+    );
+
     return (
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer">
+          <Features />
         </div>
       </div>
     );
