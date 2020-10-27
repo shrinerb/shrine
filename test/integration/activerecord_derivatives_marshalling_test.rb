2fbced2a7cbe0115ad45f95d8b4ded1906513ec3
Igor S. Morozov <igor@morozov.is>
2020-10-27 18:09:35 +0300

    Use ActiveRecord tests

--- test/integration/activerecord_derivatives_marshalling_test.rb
+++ test/integration/activerecord_derivatives_marshalling_test.rb
@@ -1,18 +1,24 @@
 require "test_helper"
-require "./test/support/sequel"
+require "./test/support/activerecord"
 
-describe "Sequel & derivatives marshalling" do
+describe "ActiveRecord & derivatives marshalling" do
   before do
     @shrine = shrine do
       plugin :sequel
       plugin :derivatives
     end
 
-    user_class = Class.new(Sequel::Model)
-    user_class.set_dataset(:users)
+    user_class = Class.new(ActiveRecord::Base)
+    user_class.table_name = :users
+    user_class.class_eval do
+      # needed for translating validation errors
+      def self.model_name
+        ActiveModel::Name.new(self, nil, "User")
+      end
+    end
     user_class.include @shrine::Attachment.new(:avatar)
 
-    @user = user_class.create
+    @user = user_class.create!
 
     # we can't Marshal anonymous classes
     Shrine::TestUploaderClass = @shrine
@@ -24,7 +30,7 @@ describe "Sequel & derivatives marshalling" do
     Shrine.send(:remove_const, :TestUploaderClass)
   end
 
-  specify "marshalling and unmarshalling don't raise any errors" do
+  specify "marshalling does not raise any errors" do
     Marshal.load(Marshal.dump(@user.avatar_attacher))
   end
 end
