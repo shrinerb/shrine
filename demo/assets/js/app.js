71d11e72b1b265b384c1804e48c72ad9013aa52d
Janko Marohnić <janko.marohnic@gmail.com>
2021-09-18 12:57:11 +0200

    Update demo app to Uppy 2
    While here, we also update JavaScript to use ES6 syntax, since all
    important browsers now support it natively.

--- demo/assets/js/app.js
+++ demo/assets/js/app.js
@@ -1,16 +1,10 @@
-// This code uses:
-//
-// * babel-polyfill (https://babeljs.io/docs/usage/polyfill/)
-// * whatwg-fetch (https://github.github.io/fetch/)
-// * uppy (https://uppy.io)
-
-function singleFileUpload(fileInput) {
-  var imagePreview = document.getElementById(fileInput.dataset.previewElement)
-  var formGroup    = fileInput.parentNode
+const singleFileUpload = (fileInput) => {
+  const imagePreview = document.getElementById(fileInput.dataset.previewElement)
+  const formGroup    = fileInput.parentNode
 
   formGroup.removeChild(fileInput)
 
-  var uppy = fileUpload(fileInput)
+  const uppy = fileUpload(fileInput)
 
   uppy
     .use(Uppy.FileInput, {
@@ -27,20 +21,18 @@ function singleFileUpload(fileInput) {
       thumbnailWidth: 600,
     })
 
-  uppy.on('upload-success', function (file, response) {
-    var uploadedFileData = window.uploadedFileData(file, response, fileInput)
-
+  uppy.on('upload-success', (file, response) => {
     // set hidden field value to the uploaded file data so that it's submitted with the form as the attachment
-    var hiddenInput = document.getElementById(fileInput.dataset.uploadResultElement)
-    hiddenInput.value = uploadedFileData
+    const hiddenInput = document.getElementById(fileInput.dataset.uploadResultElement)
+    hiddenInput.value = uploadedFileData(file, response, fileInput)
   })
 
-  uppy.on('thumbnail:generated', function (file, preview) {
+  uppy.on('thumbnail:generated', (file, preview) => {
     imagePreview.src = preview
   })
 }
 
-function multipleFileUpload(fileInput) {
+const multipleFileUpload = (fileInput) => {
   var formGroup = fileInput.parentNode
 
   var uppy = fileUpload(fileInput)
@@ -53,25 +45,25 @@ function multipleFileUpload(fileInput) {
       replaceTargetContent: true,
     })
 
-  uppy.on('upload-success', function (file, response) {
-    var hiddenField = document.createElement('input')
+  uppy.on('upload-success', (file, response) => {
+    const hiddenField = document.createElement('input')
 
     hiddenField.type = 'hidden'
     hiddenField.name = 'album[photos_attributes]['+ Math.random().toString(36).substr(2, 9) + '][image]'
-    hiddenField.value = window.uploadedFileData(file, response, fileInput)
+    hiddenField.value = uploadedFileData(file, response, fileInput)
 
     document.querySelector('form').appendChild(hiddenField)
   })
 }
 
-function fileUpload(fileInput) {
-  var uppy = Uppy.Core({
-      id: fileInput.id,
-      autoProceed: true,
-      restrictions: {
-        allowedFileTypes: fileInput.accept.split(','),
-      },
-    })
+const fileUpload = (fileInput) => {
+  const uppy = new Uppy.Core({
+    id: fileInput.id,
+    autoProceed: true,
+    restrictions: {
+      allowedFileTypes: fileInput.accept.split(','),
+    },
+  })
 
   if (fileInput.dataset.uploadServer == 's3') {
     uppy.use(Uppy.AwsS3, {
@@ -87,7 +79,7 @@ function fileUpload(fileInput) {
   return uppy
 }
 
-function uploadedFileData(file, response, fileInput) {
+const uploadedFileData = (file, response, fileInput) => {
   if (fileInput.dataset.uploadServer == 's3') {
     // construct uploaded file data in the format that Shrine expects
     return JSON.stringify({
@@ -104,7 +96,7 @@ function uploadedFileData(file, response, fileInput) {
   }
 }
 
-document.querySelectorAll('input[type=file]').forEach(function (fileInput) {
+document.querySelectorAll('input[type=file]').forEach((fileInput) => {
   if (fileInput.multiple) {
     multipleFileUpload(fileInput)
   } else {
